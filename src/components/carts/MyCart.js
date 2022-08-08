30748cbe4d7b55b22747f995a2ce3ef59054c854
RandyD <rdsoundguy@gmai.com>
2022-08-08 09:57:26 -0400

    Search works, generate cart works, add to cart works, display cart NOT WORKING

--- src/components/carts/MyCart.js
+++ src/components/carts/MyCart.js
@@ -3,10 +3,6 @@ import { useParams, useNavigate } from 'react-router-dom'
 import { Container, Card, Button } from 'react-bootstrap'
 import { removeSongFromCartApi } from '../../api/cart.js'
 import { getMyCart } from '../../api/cart.js'
-// import { useEffect } from "react/cjs/react.development";
-
-
-
 
 import Form from 'react-bootstrap/Form';
 
@@ -16,9 +12,8 @@ const MyCart = (props) => {
 
     const [cartSongsList, setCartSongsList]=useState([])
     const { msgAlert, user } = props
-
-    let song =  "TEMPORARY HOLDER. NOT ACTUAL. SHOULD BE LIKE props.songid"
-
+    const [cartItemsToShow, setCartItemsToShow] = useState([])
+    const [songListFromMongo, setSongListFromMongo]= useState({})
 
     const removeFromCartHandler = (e,mbid,user) => {
             e.preventDefault()
@@ -26,39 +21,88 @@ const MyCart = (props) => {
             removeSongFromCartApi(mbid.mbid,user)
     }
     
+    let showCards = null
+    let testCard = null
+
+    // useEffect(() => { 
+    //     getMyCart(user)
+    //     .then((res) => {
+    //         // cartItemsToDisplay = res.data.cart[0].songs
+    //         // let oneThing = res.data.cart[0].songs[9].songName // working+
+    //         console.log('^^^^^^^^^^^^IN .THEN RES^^^^^^^^^^^^^');
+    //         // setSongListFromMongo(res.data.cart[0])
+
+    //         setSongListFromMongo(songListFromMongo => ({...songListFromMongo, res }))
+
+            
+    //         // console.log('RES++++++++++++++',res.data.cart[0].songs);})
+    //     // .catch((error) => {console.log(error) })
+    //     }, [])
+    // })
+
+    
 
 
-    useEffect(() => { 
-        getMyCart(user)
-        .then((res) => {console.log('RES',res);})
-        .catch((error) => {console.log(error) })
-    }, [])
 
- 
-        
-   
+
+    const conversion = () => {
+        // let converted = JSON.parse(oneThing)
+        console.log('IN CONVERSION');
+        setSongListFromMongo("teststate")
+
+        console.log('songListFromMongo ::***********',songListFromMongo);
+            
+        return 
+
+    }
+
+    conversion()
+
+        const finalCartItems = songListFromMongo.map(cartElement => (
+            <Card style={{ width: '30%', margin: 5}} key={cartElement}>
+                <Card.Header><strong>{ cartElement }</strong></Card.Header>
+                    <Card.Body>
+                        <Card.Text>
+                            <div>{ cartElement}</div> 
+                            {/* <img src={ cartElement.image[3]['#text'] } /> */}
+                            {/* <img src={ cartElement.image[3]['#text'] } /> */}
+                            <div>{ cartElement.mbid }</div>
+                            <Form onSubmit={(e) => {removeFromCartHandler(e,cartElement,user)}} className="d-flex">
+                                <Button variant="primary" type = "submit" >
+                                    Remove From Cart        
+                                </Button>
+                            </Form>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+        ))      
   
 
     return(    
+  
         <>
-        <Card style={{ width: '30%', margin: 5}} key={song.url}>
-        
+        <Card style={{ width: '30%', margin: 5}} key={showCards}>        
          {/* should be like :<Card.Header><strong>{ song.name }</strong></Card.Header> */}
            <h2> IN MY CART</h2>
-            <Card.Header><strong>{ song }</strong></Card.Header>
+            <Card.Header><strong>{ }</strong></Card.Header>
                 <Card.Body>
                     <Card.Text>
-                        <div>{ song }</div> 
-                        <img src={ song[2]} />
-                        <div>{ song }</div>
-                        <Form onSubmit={(e) => {removeFromCartHandler(e,song,user)}} className="d-flex">
+                        <div> {}</div> 
+                        <img src={showCards} />
+                        <div>{  }</div>
+                        <Form onSubmit={(e) => {removeFromCartHandler(e,user)}} className="d-flex">
                             <Button variant="primary" type = "submit" >
-                                Remove from Cart        
+                                Remove from Cart Not working       
                             </Button>
                         </Form>
                     </Card.Text>
                 </Card.Body>
+
+                {finalCartItems}
          </Card>
+
+
+
         </>
 
     )
@@ -66,3 +110,8 @@ const MyCart = (props) => {
 }
 
 export default MyCart
+
+
+
+
+
